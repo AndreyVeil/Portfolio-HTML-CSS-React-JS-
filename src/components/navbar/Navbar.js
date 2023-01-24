c57685caaa8412db7fb643c8880a1e341f89f973
Andrei <derekheil042@gmail.com>
2023-01-24 15:21:44 +0300

    Dark theme

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,6 @@
-import sun from "./../../img/icons/sun.svg";
-import moon from "./../../img/icons/moon.svg";
-import "./style.css";
-
 import { NavLink } from "react-router-dom";
+import BtnDarkMode from "../btnDarkMode/BtnDarMode";
+import "./style.css";
 
 const Navbar = () => {
   const activeLink = "nav-list__link nav-list__link--active";
@@ -16,10 +14,7 @@ const Navbar = () => {
             <strong>Freelancer</strong> portfolio
           </NavLink>
 
-          <button className="dark-mode-btn">
-            <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-            <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-          </button>
+          <BtnDarkMode />
 
           <ul className="nav-list">
             <li className="nav-list__item">
